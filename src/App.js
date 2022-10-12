93b6b6aa83ede67da6242fbc03c760ff9b44ca09
Kitsalj <duh1296@mail.ru>
2022-10-12 22:46:46 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React from 'react'
+import volk1 from './img/2-278.jpg'
+import volk2 from './img/O3p3p1651811755.jpg'
+import volk3 from './img/15259645_144_0_1424_1280_1920x0_80_0_0_9506a48af40eb81f05e789e64e467ac7.jpg'
+
+const masVolk = [volk1, volk2, volk3]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setsSate] = React.useState(0)
+    const [inputValue, changeInputValue] = React.useState('')
+    const [volkNow, drVolk] = React.useState(0)
+
+    const increase = () => {
+        let current = state
+        setsSate(++current)
+    }
+
+    const decrease = () => {
+        let current = state
+        setsSate(--current)
+    }
+
+    const ok = () => {
+        setsSate(state + Number(inputValue))
+    }
+
+    const changeValue = (event) => {
+        changeInputValue(event.target.value)
+    }
+    const nextSlide = () => {
+        drVolk((current)=> {
+            if (current < masVolk.length -1) {
+                return ++current
+            }
+            return 0
+        })
+    }
+    const prevSlide = () => {
+
+    }
+
+    return (
+        <div>
+            {/*<h1>{state}</h1>*/}
+            {/*<input type={"number"} onChange={(event) => changeValue(event)}/>*/}
+            {/*<button onClick={increase}>+</button>*/}
+            {/*<button onClick={decrease}>-</button>*/}
+            {/*<button onClick={ok}>OK</button>*/}
+            {/*<button onClick={startCounter}>Start</button>*/}
+            {/*<button onClick={stopCounter}>Stop</button>*/}
+
+            <div className='slider'>
+                <div className='sliders'>
+                    <div className='slide1'>
+                        <img src={masVolk[volkNow]}/>
+                    </div>
+                </div>
+                    <div className='buttons'>
+                        <button onClick={nextSlide}>Next Volk</button>
+                        <button onClick={prevSlide}>Prev Volk</button>
+                    </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
